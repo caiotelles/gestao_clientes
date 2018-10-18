e64ebca6c18f3c22bf082d6de0d5aa0298f17486
Caio Telles <caiotelles@idrwxr18.local>
2018-10-18 14:18:10 -0300

    Personalizacao de modelos de relacionamento com BD

--- clientes/admin.py
+++ clientes/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from .models import Pessoas, Documento, Venda, Produto
+from .models import Pessoas, Venda, Produto
 
 # Register your models here.
 
 
 admin.site.register(Pessoas)
-admin.site.register(Documento)
+"""admin.site.register(Documento)"""
 admin.site.register(Venda)
 admin.site.register(Produto)
--- clientes/models.py
+++ clientes/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
-class Documento(models.Model):
+"""class Documento(models.Model):
     num_doc = models.CharField(max_length=20)
 
     def __str__(self):
-        return self.num_doc
+        return self.num_doc"""
 
 class Pessoas(models.Model):
     nome = models.CharField(max_length=30)
@@ -13,26 +13,26 @@ class Pessoas(models.Model):
     salario = models.DecimalField(max_digits=10, decimal_places=2)
     bio = models.TextField()
     foto = models.ImageField(upload_to='fotos_pessoas', null=True, blank=True)
-    doc = models.OneToOneField(Documento, null=True, blank=True, on_delete=models.CASCADE)
+    """ doc = models.OneToOneField(Documento, null=True, blank=True, on_delete=models.CASCADE)"""
 
-    def __str__(self):
-        return self.nome + ' ' + self.sobrenome
+  def __str__(self):
+      return self.nome + ' ' + self.sobrenome
 
 
 class Produto(models.Model):
-    descricao = models.CharField(max_length=100)
-    preco = models.DecimalField(max_digits=5, decimal_places=2)
+  descricao = models.CharField(max_length=100)
+  preco = models.DecimalField(max_digits=5, decimal_places=2)
 
-    def __str__(self):
-        return self.descricao
+  def __str__(self):
+      return self.descricao
 
 class Venda(models.Model):
-    numero = models.CharField(max_length=7)
-    valor = models.DecimalField(max_digits=5, decimal_places=2)
-    desconto = models.DecimalField(max_digits=5, decimal_places=2)
-    impostos = models.DecimalField(max_digits=5, decimal_places=2)
-    pessoa = models.ForeignKey(Pessoas, null=True, blank=True, on_delete=models.PROTECT)
-    produtos = models.ManyToManyField(Produto, blank=True)
+  numero = models.CharField(max_length=7)
+  valor = models.DecimalField(max_digits=5, decimal_places=2)
+  desconto = models.DecimalField(max_digits=5, decimal_places=2)
+  impostos = models.DecimalField(max_digits=5, decimal_places=2)
+  pessoa = models.ForeignKey(Pessoas, null=True, blank=True, on_delete=models.PROTECT)
+  produtos = models.ManyToManyField(Produto, blank=True)
 
-    def __str__(self):
-        return self.numero
+  def __str__(self):
+      return self.numero
